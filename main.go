d53a2cd6f6e67924c9d9a4aecc6af384cc6ce69d
Pedro Herrera <111007288+Pedroherreraso@users.noreply.github.com>
2022-09-27 08:57:17 -0300

    Add files via upload
    Creacion de la interfaz mediante la terminal. Funciona todo menos el login(lo cual impide probar compra) pero todo lo demas funciona

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"API-E-commerce/server"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type Todo struct {
+	Nombre              string `json:"nombre"`
+	Cantidad_Disponible int    `json:"cantidad_disponible"`
+	Precio_Unitario     int    `json:"precio_unitario"`
+}
+type Client struct {
+	Id       int
+	Password string
+}
 
 func main() {
 	go server.Server()
@@ -26,18 +41,116 @@ func terminalInterface() {
 	fmt.Scan(&firstOption)
 
 	if firstOption == "1" {
-		var userId string
+		var userId int
 		var userPass string
 		fmt.Print("Ingrese su id: ")
 		fmt.Scan(&userId)
 		fmt.Print("Ingrese su contraseña: ")
 		fmt.Scan(&userPass)
-		//llamar a la api y ver si el usuario existe
+		todo := Client{userId, userPass}
+		jsonReq, err := json.Marshal(todo)
+		resp, err := http.Post("http://localhost:5000/api/clientes/iniciar_sesion", "application/json; charset=utf-8", bytes.NewBuffer(jsonReq))
+		if err != nil {
+			fmt.Print("Error, no hay ninguna coincidencia con los datos ingresados.")
+
+		}
+		defer resp.Body.Close()
+		fmt.Println("Inicio de sesión exitoso")
+		fmt.Println("Opciones:")
+		fmt.Println("1. Ver lista de productos")
+		fmt.Println("2. Hacer compra")
+		fmt.Println("3. Salir")
+		var secondOption string
+		fmt.Print("Ingrese una opción: ")
+		fmt.Scan(&secondOption)
+		if secondOption == "1" {
+			resp, err := http.Get("http://localhost:5000/api/productos")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer resp.Body.Close()
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+			// Convert response body to string
+			bodyString := string(bodyBytes)
+			fmt.Println("API Response as String:\n" + bodyString)
+		} else if secondOption == "2" {
+
+		} else if secondOption == "3" {
+			fmt.Print("Hasta luego!")
+		}
+
 	} else if firstOption == "2" {
 		var adminPass string
 		fmt.Println("Ingrese contraseña de administrador: ")
 		fmt.Scan(&adminPass)
+		fmt.Println("Opciones:")
+		fmt.Println("1. Ver lista de productos")
+		fmt.Println("2. Crear producto")
+		fmt.Println("3. Eliminar producto")
+		fmt.Println("4. Ver estadísticas")
+		fmt.Println("5. Salir")
+		var secondOption string
+		fmt.Print("Ingrese una opción: ")
+		fmt.Scan(&secondOption)
+		if secondOption == "1" {
+			resp, err := http.Get("http://localhost:5000/api/productos")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer resp.Body.Close()
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+			// Convert response body to string
+			bodyString := string(bodyBytes)
+			fmt.Println("API Response as String:\n" + bodyString)
+		} else if secondOption == "2" {
+			var name string
+			var cantidad int
+			var precio int
+			fmt.Scan(&name)
+			fmt.Scan(&cantidad)
+			fmt.Scan(&precio)
+			todo := Todo{name, cantidad, precio}
+			jsonReq, err := json.Marshal(todo)
+			resp, err := http.Post("http://localhost:5000/api/productos", "application/json; charset=utf-8", bytes.NewBuffer(jsonReq))
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer resp.Body.Close()
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+			// Convert response body to string
+			bodyString := string(bodyBytes)
+			fmt.Println(bodyString)
+		} else if secondOption == "3" {
+			var id string
+			fmt.Println("Ingrese id a eliminar ")
+			fmt.Scan(&id)
+			todo := "1"
+			jsonReq, err := json.Marshal(todo)
+			req, err := http.NewRequest(http.MethodDelete, "http://localhost:5000/api/productos/"+id, bytes.NewBuffer(jsonReq))
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			defer resp.Body.Close()
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+			// Convert response body to string
+			bodyString := string(bodyBytes)
+			fmt.Println(bodyString)
+		} else if secondOption == "4" {
+
+		} else if secondOption == "5" {
+			fmt.Print("Hasta luego!")
+		}
 	} else if firstOption == "3" {
-		fmt.Println("eligio la opcion 3")
+		fmt.Println("Hasta luego!")
 	}
 }
